07062baaa960ad76828345f50a9cf4fabc670c26
Ephraim Reifel <reifel@post.bgu.ac.il>
2021-11-25 15:51:10 +0200

    Semi stable update to better graphics

--- Dynamic model.py
+++ Dynamic model.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Yp = 100
-pi_p = pi_t = 0.02  # pi_p is pi past and pi_t is pi target
+pi_p = pi_t = 0.02  # pi_p is past pi and pi_t is target pi
 phi = 0.25
 vt = 0
 a = 1  # alpha
@@ -11,14 +11,10 @@ tt_pi = 0.5
 ro = 0.02
 e = 0
 
-fig, ax = plt.subplots(figsize=(12, 8))
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
+fig, axs = plt.subplots(1, 3)
 
 # -- Default or manual --
-if not (input("Do you want to use the default values? (y,n) ") == "y"):
+if not (input("Do you want to use the default values? ('y' - yes, else - no) ") == "y"):
     Yp = float(input("Insert potential Yield (default = 100): "))
     pi_p = float(input("Insert previous inflation (default = 0.02): "))
     pi_t = float(input("Insert target inflation (default = 0.02): "))
@@ -31,6 +27,7 @@ if not (input("Do you want to use the default values? (y,n) ") == "y"):
     tt_pi = float(input("Insert the inflation's reaction to the inflation not being equal to the target inflation ("
                         "default = 0.5): "))
 
+
 # -- Defining functions --
 def Pi_s(Y):
     return pi_p + phi * (Y - Yp) + vt
@@ -42,27 +39,43 @@ def Pi_d(Y):
 
 Y = np.arange(0, 200, 0.001)
 
-# -- DAS initial plot --
-pi_s = pi_p + phi * (Y - Yp) + vt
-plt.plot(Y, pi_s, label="DAS")
 
-# -- DAD initial plot --
-pi_d = pi_t + ((1 + a * tt_y) / (a * tt_pi)) * (Yp - Y) + (1 / (a * tt_pi)) * e
-plt.plot(Y, pi_d, label="DAD")
-
-# -- Demand Shock --
-shock = float(input("Please enter a demand shock (0 if none):"))
-if shock != 0:
-    e = shock
-    pi_d = pi_t + ((1 + a * tt_y) / (a * tt_pi)) * (Yp - Y) + (1 / (a * tt_pi)) * e
-    plt.plot(Y, pi_d, label="DAD-Shocked")
-
-# -- Supply Shock --
-shock = float(input("Please enter a Supply shock (0 if none):"))
-if shock != 0:
-    vt = shock
+# -- t_0 base plot --
+def plotbasefunc(i):  # function that plots the base supply and demand plots
+    # -- DAS initial plot --
+    vt = 0
     pi_s = pi_p + phi * (Y - Yp) + vt
-    plt.plot(Y, pi_s, label="DAS-Shocked")
+    axs[i] = plt.plot(Y, pi_s, 'o', label="DAS")
+
+    # -- DAD initial plot --
+    e = 0
+    pi_d = pi_t + ((1 + a * tt_y) / (a * tt_pi)) * (Yp - Y) + (1 / (a * tt_pi)) * e
+    axs[i] = plt.plot(Y, pi_d, 'o',  label="DAD")
+
+
+# -- Calling to plot --
+axs[0] = plt.figure(0)
+plotbasefunc(0)
+
+# -- Asking for shocks --
+shockd = float(input("Please enter a Demand shock (0 if none):"))
+shocks = float(input("Please enter a Supply shock (0 if none):"))
+
+# -- Plotting the shocks --
+for i in 1, 2:
+    axs[i] = plt.figure(i)
+    plotbasefunc(i)
+    # -- Demand Shock calc--
+    if shockd != 0:
+        e = shockd
+        pi_d = pi_t + ((1 + a * tt_y) / (a * tt_pi)) * (Yp - Y) + (1 / (a * tt_pi)) * e
+        axs[i] = plt.plot(Y, pi_d, label="DAD-Shocked")
+
+    # -- Supply Shock calc--
+    if shocks != 0:
+        vt = shocks
+        pi_s = pi_p + phi * (Y - Yp) + vt
+        axs[i] = plt.plot(Y, pi_s, label="DAS-Shocked")
 
 # -- Initial cross point --
 lY = 0
@@ -72,22 +85,22 @@ for Yi in Y:
         d = abs(Pi_s(Yi) - Pi_d(Yi))
         lY = Yi
 print("The Intial Cross Point Is: Y=", lY, " pi=", Pi_d(lY))
-iY=lY
+iY = lY
 pi_p = Pi_d(lY)
 
 # -- Moving towards equilibrium --
+axs[2] = plt.figure(2)
 runs = 0
 if e != 0 or vt != 0:
     print("There was a Shock, we'll need to move to an equilibrium")
-e = 0
-vt = 0
+e = vt = 0
 timelimit = int(input("Do you want a time limit to checking the equilibrium? (3 is recommended): "))
 while abs(lY - Yp) > 0.01 and runs < timelimit:
     runs = runs + 1
     pi_s = pi_p + phi * (Y - Yp) + vt
-    plt.plot(Y, pi_s, label="DAS" + str(runs))
+    axs[2] = plt.plot(Y, pi_s, label="DAS" + str(runs))
     pi_d = pi_t + ((1 + a * tt_y) / (a * tt_pi)) * (Yp - Y) + (1 / (a * tt_pi)) * e
-    plt.plot(Y, pi_d, label="DAD" + str(runs))
+    axs[2] = plt.plot(Y, pi_d, label="DAD" + str(runs))
     d = 100
     for Yi in Y:
         if (abs(Pi_s(Yi) - Pi_d(Yi))) < d:
@@ -105,21 +118,22 @@ print("The Final Cross Point Is: Y=", lY, " pi=", Pi_d(lY), " and it took me ", 
 show = input("Wanna see the graph? (y,n) ")
 if show == "y":
     lx = ry = uy = dy = 0
-    if iY >= Yp :
-        rx = iY+0.1
-        lx = Yp-0.1
+    if iY >= Yp:
+        rx = iY + 0.1
+        lx = Yp - 0.1
     else:
-        lx = iY-0.1
-        rx = Yp+0.1
+        lx = iY - 0.1
+        rx = Yp + 0.1
     if Pi_d(iY) >= Pi_d(lY):
-        uy = Pi_d(iY)+0.2
-        dy = Pi_d(lY)-0.2
+        uy = Pi_d(iY) + 0.2
+        dy = Pi_d(lY) - 0.2
     else:
-        uy = Pi_d(lY)+0.2
-        dy = Pi_d(iY)-0.2
-    plt.ylim([dy, uy])
-    plt.xlim([lx, rx])
-    plt.grid()
-    plt.legend()
+        uy = Pi_d(lY) + 0.2
+        dy = Pi_d(iY) - 0.2
+    for plo in 0, 1, 2:
+        plt.figure(plo)
+        plt.ylim([dy, uy])
+        plt.xlim([lx, rx])
+        plt.grid()
+        plt.legend()
     plt.show()
-    plt.pause(0.8)
